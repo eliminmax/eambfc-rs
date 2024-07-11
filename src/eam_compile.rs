00f8a3ad85b531d7fa6b443979f6df81c26b3b7f
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-07-11 17:42:58 -0400

    bf_compile takes RunConfig as argument

--- src/eam_compile.rs
+++ src/eam_compile.rs
@@ -1,9 +1,15 @@
 // SPDX-FileCopyrightText: 2024 Eli Array Minkoff
 //
 // SPDX-License-Identifier: GPL-3.0-only
-use std::fs::File;
 use super::err::BFCompileError;
+use super::run_config::RunConfig;
+use std::fs::File;
 
-pub fn bf_compile(in_f: &mut File, out_f: &mut File, optimize: bool) -> Result<(), BFCompileError> {
+pub fn bf_compile(
+    in_f: &mut File,
+    out_f: &mut File,
+    optimize: bool,
+    rc: &RunConfig,
+) -> Result<(), BFCompileError> {
     todo!("bf_compile");
 }
