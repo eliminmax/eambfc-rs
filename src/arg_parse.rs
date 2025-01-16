824bdc088762b46db164a788696ec3aff40ab70e
Eli Array Minkoff <eli@planetminkoff.com>
2025-01-16 01:15:15 -0500

    Rework arg_parse::parse_args
    Still an ugly behemoth of a function, but less than it was

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -28,6 +28,154 @@ pub enum RunConfig {
     ShowVersion(String),
     ShowArches(String),
 }
+
+fn parameter_instr<T: Iterator<Item = OsString>>(
+    flag: u8,
+    remainder: Vec<u8>,
+    args: &mut T,
+) -> Result<OsString, BFCompileError> {
+    if remainder.is_empty() {
+        args.next().ok_or_else(|| {
+            BFCompileError::basic(
+                BFErrorID::MISSING_OPERAND,
+                format!("-{} requires an additional argument", flag.escape_ascii()),
+            )
+        })
+    } else {
+        Ok(OsString::from_vec(remainder))
+    }
+}
+
+#[allow(clippy::too_many_lines)]
+pub fn parse_args<T: Iterator<Item = OsString>>(
+    mut args: T,
+) -> Result<RunConfig, (BFCompileError, String, OutMode)> {
+    // argument 0 should be the name of the file.
+    // if not present, it's sensible to fall back to a sane default of "eambfc-rs".
+    let progname = args.next().unwrap_or(OsString::from("eambfc-rs"));
+    let progname = progname.to_string_lossy().to_string();
+    let mut extension: Option<OsString> = None;
+    let mut source_files = Vec::<OsString>::new();
+    let mut out_mode = OutMode::Basic;
+    let mut optimize = false;
+    let mut keep = false;
+    let mut cont = false;
+    let mut tape_blocks: Option<u64> = None;
+    let mut arch: Option<ELFArch> = None;
+
+    macro_rules! error_out {
+        ($error_type: ident, $msg: expr) => {{
+            return Err((
+                BFCompileError::basic(BFErrorID::$error_type, $msg),
+                progname,
+                out_mode,
+            ));
+        }};
+    }
+    while let Some(arg) = args.next() {
+        // handle non-flag values
+        if arg == "--" {
+            source_files.extend(args);
+            break;
+        }
+        let arg_bytes = arg.into_vec();
+        if arg_bytes[0] != b'-' {
+            source_files.push(OsString::from_vec(arg_bytes));
+            source_files.extend(args);
+            break;
+        }
+        let mut arg_byte_iter = arg_bytes.into_iter().skip(1);
+        while let Some(b) = arg_byte_iter.next() {
+            macro_rules! parameter_instr {
+                ($flag: literal) => {{
+                    match parameter_instr($flag, arg_byte_iter.collect(), &mut args) {
+                        Ok(o) => o,
+                        Err(e) => return Err((e, progname, out_mode)),
+                    }
+                }};
+            }
+            match b {
+                b'a' => {
+                    if arch.is_some() {
+                        error_out!(MULTIPLE_ARCHES, "passed -a multiple times");
+                    }
+                    arch = match parameter_instr!(b'a').as_bytes() {
+                        #[cfg(feature = "x86_64")]
+                        b"x86_64" | b"x64" | b"amd64" | b"x86-64" => Some(ELFArch::X86_64),
+                        #[cfg(feature = "arm64")]
+                        b"arm64" | b"aarch64" => Some(ELFArch::Arm64),
+                        #[cfg(feature = "s390x")]
+                        b"s390x" | b"s390" | b"z/architecture" => Some(ELFArch::S390x),
+                        f => error_out!(
+                            UNKNOWN_ARCH,
+                            format!(
+                                "{} is not a recognized architecture",
+                                OsStr::from_bytes(f).to_string_lossy()
+                            )
+                        ),
+                    };
+                    break;
+                }
+                b'e' => {
+                    if extension.is_some() {
+                        error_out!(MULTIPLE_EXTENSIONS, "passed -e multiple times");
+                    }
+                    extension = Some(parameter_instr!(b'e'));
+                    break;
+                }
+                b't' => {
+                    if tape_blocks.is_some() {
+                        error_out!(MULTIPLE_TAPE_BLOCK_COUNTS, "passed -t multiple times");
+                    }
+                    match parameter_instr!(b't').to_string_lossy().parse::<u64>() {
+                        Ok(0) => error_out!(NO_TAPE, "Tape value for -t must be at least 1."),
+                        Ok(i) if i >= u64::MAX >> 12 => error_out!(
+                            TAPE_TOO_LARGE,
+                            format!("{i} * 0x1000 exceeds the 64-bit integer limit.")
+                        ),
+                        Ok(i) => tape_blocks = Some(i),
+                        Err(s) => error_out!(
+                            NOT_NUMERIC,
+                            format!("{s} could not be parsed as a numeric value")
+                        ),
+                    }
+                    break;
+                }
+                b'h' => return Ok(RunConfig::ShowHelp(progname)),
+                b'V' => return Ok(RunConfig::ShowVersion(progname)),
+                b'A' => return Ok(RunConfig::ShowArches(progname)),
+                b'j' => out_mode = OutMode::JSON,
+                // for consistency with original C version, quiet doesn't override JSON mode
+                b'q' => {
+                    if out_mode == OutMode::Basic {
+                        out_mode = OutMode::Quiet;
+                    }
+                }
+                b'O' => optimize = true,
+                b'k' => keep = true,
+                b'c' => cont = true,
+                c => return Err((BFCompileError::unknown_flag(c), progname, out_mode)),
+            };
+        }
+    }
+    if source_files.is_empty() {
+        error_out!(NO_SOURCE_FILES, "No source files provided");
+    }
+
+    Ok(RunConfig::StandardRun(StandardRunConfig {
+        progname,
+        out_mode,
+        optimize,
+        keep,
+        cont,
+        tape_blocks: tape_blocks.unwrap_or(8),
+        extension: extension.unwrap_or(".bf".into()),
+        source_files,
+        arch: arch.unwrap_or_default(),
+    }))
+}
+
+#[cfg(test)]
 impl Default for StandardRunConfig {
     fn default() -> Self {
         StandardRunConfig {
@@ -42,209 +190,6 @@ impl Default for StandardRunConfig {
             arch: ELFArch::default(),
         }
     }
-}
-
-pub fn parse_args<T: Iterator<Item = OsString>>(
-    mut args: T,
-) -> Result<RunConfig, (BFCompileError, String, OutMode)> {
-    // argument 0 should be the name of the file.
-    // if not present, it's sensible to fall back to a sane default of "eambfc-rs".
-    let progname = args.next().unwrap_or(OsString::from("eambfc-rs"));
-    let progname = progname.to_string_lossy().to_string();
-
-    let mut extension = OsString::new();
-    let mut source_files = Vec::<OsString>::new();
-    let mut out_mode = OutMode::Basic;
-    let mut optimize = false;
-    let mut keep = false;
-    let mut cont = false;
-    let mut tape_blocks: Option<u64> = None;
-    let mut arch: Option<ELFArch> = None;
-    while let Some(arg) = args.next() {
-        // some logic to treat anything after `--` as literal values
-        if arg == "--" {
-            source_files.extend(args);
-            break;
-        }
-        let arg_bytes = arg.into_vec();
-        if arg_bytes[0] == b'-' {
-            let mut arg_byte_iter = arg_bytes.into_iter().skip(1);
-            while let Some(b) = arg_byte_iter.next() {
-                // define macro here so that all of the values it references are in scope
-                // this macro is just because -e and -t have the following in common in
-                // their implementation
-                macro_rules! parameter_instr {
-                    ($flag: literal) => {{
-                        let remainder = arg_byte_iter.collect::<Vec<u8>>();
-                        if remainder.len() > 0 {
-                            OsString::from_vec(remainder)
-                        } else {
-                            if let Some(next_arg) = args.next() {
-                                next_arg
-                            } else {
-                                return Err((
-                                    BFCompileError::basic(
-                                        BFErrorID::MISSING_OPERAND,
-                                        format!(
-                                            "-\"{}\" requires an additional argument",
-                                            $flag.escape_ascii(),
-                                        ),
-                                    ),
-                                    progname,
-                                    out_mode,
-                                ));
-                            }
-                        }
-                    }};
-                }
-                match b {
-                    b'a' => {
-                        if arch.is_some() {
-                            return Err((
-                                BFCompileError::basic(
-                                    BFErrorID::MULTIPLE_ARCHES,
-                                    String::from("passed -a multiple times"),
-                                ),
-                                progname,
-                                out_mode,
-                            ));
-                        }
-                        match parameter_instr!(b'a').as_bytes() {
-                            #[cfg(feature = "x86_64")]
-                            b"x86_64" | b"x64" | b"amd64" | b"x86-64" => {
-                                arch = Some(ELFArch::X86_64);
-                            }
-                            #[cfg(feature = "arm64")]
-                            b"arm64" | b"aarch64" => arch = Some(ELFArch::Arm64),
-                            #[cfg(feature = "s390x")]
-                            b"s390x" | b"s390" | b"z/architecture" => arch = Some(ELFArch::S390x),
-                            f => {
-                                return Err((
-                                    BFCompileError::basic(
-                                        BFErrorID::UNKNOWN_ARCH,
-                                        format!(
-                                            "{} is not a recognized architecture",
-                                            OsStr::from_bytes(f).to_string_lossy()
-                                        ),
-                                    ),
-                                    progname,
-                                    out_mode,
-                                ))
-                            }
-                        }
-                        break;
-                    }
-                    b'e' => {
-                        if !extension.is_empty() {
-                            return Err((
-                                BFCompileError::basic(
-                                    BFErrorID::MULTIPLE_EXTENSIONS,
-                                    "passed -e multiple times",
-                                ),
-                                progname,
-                                out_mode,
-                            ));
-                        }
-                        extension = parameter_instr!(b'e');
-                        break;
-                    }
-                    b't' => {
-                        if tape_blocks.is_some() {
-                            return Err((
-                                BFCompileError::basic(
-                                    BFErrorID::MULTIPLE_TAPE_BLOCK_COUNTS,
-                                    "passed -t multiple times",
-                                ),
-                                progname,
-                                out_mode,
-                            ));
-                        }
-                        match parameter_instr!(b't').to_string_lossy().parse::<u64>() {
-                            Ok(0) => {
-                                return Err((
-                                    BFCompileError::basic(
-                                        BFErrorID::NO_TAPE,
-                                        "Tape value for -t must be at least 1.",
-                                    ),
-                                    progname,
-                                    out_mode,
-                                ));
-                            }
-                            // if it's any larger than this, the tape size would exceed the 64-bit
-                            // integer limit.
-                            Ok(i) if i >= u64::MAX >> 12 => {
-                                return Err((
-                                    BFCompileError::basic(
-                                        BFErrorID::TAPE_TOO_LARGE,
-                                        format!("{i} * 0x1000 exceeds the 64-bit integer limit."),
-                                    ),
-                                    progname,
-                                    out_mode,
-                                ));
-                            }
-                            Ok(i) => tape_blocks = Some(i),
-                            Err(s) => {
-                                return Err((
-                                    BFCompileError::basic(
-                                        BFErrorID::NOT_NUMERIC,
-                                        format!("{s} could not be parsed as a numeric value"),
-                                    ),
-                                    progname,
-                                    out_mode,
-                                ));
-                            }
-                        }
-                        break;
-                    }
-                    b'h' => return Ok(RunConfig::ShowHelp(progname.to_string())),
-                    b'V' => return Ok(RunConfig::ShowVersion(progname.to_string())),
-                    b'A' => return Ok(RunConfig::ShowArches(progname.to_string())),
-                    b'j' => out_mode = OutMode::JSON,
-                    // for consistency with original C version, quiet doesn't override JSON mode
-                    b'q' => {
-                        if out_mode == OutMode::Basic {
-                            out_mode = OutMode::Quiet;
-                        }
-                    }
-                    b'O' => optimize = true,
-                    b'k' => keep = true,
-                    b'c' => cont = true,
-                    c => return Err((BFCompileError::unknown_flag(c), progname, out_mode)),
-                };
-            }
-        } else {
-            // not an option flag, so convert back to OsString then save current and remaining args
-            // as brainfuck source files.
-            source_files.push(OsString::from_vec(arg_bytes));
-            source_files.extend(args);
-            break;
-        }
-    }
-    if source_files.is_empty() {
-        return Err((
-            BFCompileError::basic(BFErrorID::NO_SOURCE_FILES, "No source files provided"),
-            progname,
-            out_mode,
-        ));
-    }
-    // fall back to default extension/tape size if none was provided
-    if extension.is_empty() {
-        extension = OsString::from(".bf");
-    }
-    let tape_blocks = tape_blocks.unwrap_or(8);
-    let arch = arch.unwrap_or_default();
-
-    Ok(RunConfig::StandardRun(StandardRunConfig {
-        progname,
-        out_mode,
-        optimize,
-        keep,
-        cont,
-        tape_blocks,
-        extension,
-        source_files,
-        arch,
-    }))
 }
 
 #[cfg(test)]
