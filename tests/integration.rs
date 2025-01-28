2d567c4ffac5917b28b862c8295a78b92e549726
Eli Array Minkoff <eli@planetminkoff.com>
2025-01-28 15:32:07 -0500

    Run tests in temporary directory
    It was hard to work out how to set that up and automatically clean up
    when done, but with the help of https://stackoverflow.com/a/71953790, I
    managed.

--- tests/integration.rs
+++ tests/integration.rs
@@ -6,26 +6,80 @@
 mod integration_tests {
     extern crate serde;
     extern crate serde_json;
+    extern crate tempfile;
     use serde::Deserialize;
+    use std::fs;
+    use std::path::{Path, PathBuf};
     use std::process::Command;
+    use std::sync::OnceLock;
+    use tempfile::TempDir;
+    use static_init::dynamic;
+
 
     const PATH: &str = "./target/debug/eambfc-rs";
 
+    const TEST_FILES: [&str; 11] = [
+        "alternative_extension.brnfck",
+        "colortest.bf",
+        "hello.bf",
+        "loop.bf",
+        "null.bf",
+        "rw.bf",
+        "truthmachine.bf",
+        "unmatched_close.bf",
+        "unmatched_open.bf",
+        "wrap2.bf",
+        "wrap.bf",
+    ];
+
+    /// Return the working directory. The first time it's called, it's initialized with `TempDir`,
+    /// and subdirectories for each testable arches are created, with the source files for each test
+    /// copied into them.
+    fn working_dir() -> impl std::ops::Deref<Target = TempDir> {
+        #[dynamic(lazy, drop)]
+        static mut WORKING_DIR: TempDir = TempDir::new().unwrap();
+        fn init_arch_dir(dst: impl AsRef<Path>) {
+            fs::create_dir(&dst).unwrap();
+            for file in TEST_FILES.iter().copied() {
+                fs::copy(
+                    PathBuf::from("./test_assets/templates").join(file),
+                    dst.as_ref().join(file),
+                )
+                .unwrap();
+            }
+        }
+        static INIT: OnceLock<()> = OnceLock::new();
+        INIT.get_or_init(|| {
+            let inner = tempfile::TempDir::new().unwrap();
+            #[cfg(can_run_arm64)]
+            init_arch_dir(inner.path().join("arm64"));
+            #[cfg(can_run_s390x)]
+            init_arch_dir(inner.path().join("s390x"));
+            #[cfg(can_run_x86_64)]
+            init_arch_dir(inner.path().join("x86_64"));
+        });
+        WORKING_DIR.read()
+    }
+
+    fn test_asset(sub_path: impl AsRef<Path>) -> PathBuf {
+        working_dir().path().join(sub_path)
+    }
+
     macro_rules! eambfc_with_args {
+        (with_cmd $cmd:expr, $arg:expr) => {
+            $cmd.arg($arg)
+        };
+        (with_cmd $cmd:expr, $arg:expr, $($args:expr),*) => {
+            eambfc_with_args!(with_cmd $cmd.arg($arg), $($args),*)
+        };
         () => {
             Command::new(PATH)
         };
-        ($arg:literal) => {
+        ($arg:expr) => {
             eambfc_with_args!().arg($arg)
         };
-        ($arg:literal  $($args:literal )+) => {
-            eambfc_with_args!(eambfc_with_args!().arg($arg), $($args )+)
-        };
-        ($cmd:expr, $arg:literal) => {
-            $cmd.arg($arg)
-        };
-        ($cmd:expr, $arg:literal  $($args:literal )+) => {
-            eambfc_with_args!($cmd.arg($arg), $($args )+)
+        ($arg:expr, $($args:expr),*) => {
+            eambfc_with_args!(with_cmd eambfc_with_args!().arg($arg), $($args),*)
         };
     }
 
@@ -35,7 +89,6 @@ mod integration_tests {
     )]
     #[derive(Deserialize, Debug, PartialEq, Clone)]
     struct ErrorMsg {
-        #[deny(unused)]
         errorId: String,
         message: String,
         instruction: Option<String>,
@@ -67,7 +120,8 @@ mod integration_tests {
             output = output.replace(
                 &format!(
                     include_str!("../src/text_assets/help_template.txt"),
-                    PATH, "x86_64"
+                    PATH,
+                    env!("EAMBFC_DEFAULT_ARCH")
                 ),
                 "",
             );
@@ -77,13 +131,13 @@ mod integration_tests {
     }
 
     macro_rules! test_err {
-        ($first_err: literal, $($args: literal )*) => {
+        ($first_err: expr, $($args:expr),+) => {
             let errors = String::from_utf8(
-                eambfc_with_args!("-j" $($args )*).output().unwrap().stdout
+                eambfc_with_args!("-j", $($args),+).output().unwrap().stdout
                 ).unwrap().lines().map(|e| serde_json::from_str(&e).unwrap()).collect::<Vec<_>>();
             ErrorMsg::validate_formatting(
                 &errors,
-                eambfc_with_args!($($args )*)
+                eambfc_with_args!($($args),+)
             );
             assert_eq!(errors[0].errorId, $first_err);
         };
@@ -91,23 +145,23 @@ mod integration_tests {
 
     #[test]
     fn test_simple_errors() {
-        test_err!("MULTIPLE_EXTENSIONS", "-e" ".bf" "-e" ".b");
-        test_err!("MULTIPLE_TAPE_BLOCK_COUNTS", "-t" "32" "-t" "76");
+        test_err!("MULTIPLE_EXTENSIONS", "-e", ".bf", "-e", ".b");
+        test_err!("MULTIPLE_TAPE_BLOCK_COUNTS", "-t", "32", "-t", "76");
         test_err!("MISSING_OPERAND", "-t");
         test_err!("UNKNOWN_ARG", "-r");
         test_err!("NO_SOURCE_FILES", "-k");
         test_err!("BAD_EXTENSION", "e");
-        test_err!("NO_TAPE", "-t" "0");
+        test_err!("NO_TAPE", "-t", "0");
         test_err!("TAPE_TOO_LARGE", "-t9223372036854775807");
-        test_err!("NOT_NUMERIC", "-t" "hello");
+        test_err!("NOT_NUMERIC", "-t", "hello");
         test_err!("OPEN_R_FAILED", "nonexistent.bf");
-        test_err!("UNKNOWN_ARCH", "-a" "pdp10.99999");
-        test_err!("MULTIPLE_ARCHES", "-a" "amd64" "-aarm64");
+        test_err!("UNKNOWN_ARCH", "-a", "pdp10.99999");
+        test_err!("MULTIPLE_ARCHES", "-a", "amd64", "-aarm64");
     }
 
     #[test]
     fn quiet_means_quiet() {
-        let cmd_output = eambfc_with_args!("-q" "these" "are" "quite" "bad" "args" "-t0")
+        let cmd_output = eambfc_with_args!("-q", "these", "are", "quite", "bad", "args", "-t0")
             .output()
             .unwrap();
         assert!(cmd_output.stdout.is_empty());
@@ -117,8 +171,8 @@ mod integration_tests {
     #[cfg(unix)]
     #[test]
     fn permission_error_test() -> std::io::Result<()> {
-        use std::fs::{copy as copy_file, remove_file, set_permissions, OpenOptions, Permissions};
-        use std::os::unix::fs::{OpenOptionsExt, PermissionsExt};
+        use fs::{copy as copy_file, OpenOptions};
+        use std::os::unix::fs::OpenOptionsExt;
 
         let mut open_options = OpenOptions::new();
         open_options
@@ -126,23 +180,24 @@ mod integration_tests {
             .create(true)
             .truncate(false)
             .mode(0o044);
-        drop(open_options.open("test_assets/unreadable.bf")?);
-        test_err!("OPEN_R_FAILED", "test_assets/unreadable.bf");
-        set_permissions("test_assets/unreadable.bf", Permissions::from_mode(0o644))?;
-        remove_file("test_assets/unreadable.bf")?;
-        assert!(!std::fs::exists("test_assets/unreadable")?);
+        let unreadable_src = test_asset("unreadable.bf");
+        drop(open_options.open(&unreadable_src)?);
+        test_err!("OPEN_R_FAILED", unreadable_src.as_os_str().to_str().unwrap());
 
-        copy_file("test_assets/templates/hello.bf", "test_assets/unwritable.bf")?;
+        let unwritable_dest = test_asset("unwritable");
+        let unwritable_src = test_asset("unwritable.bf");
+        copy_file(
+            "test_assets/templates/hello.bf",
+            test_asset("unwritable.bf"),
+        )?;
         let mut open_options = OpenOptions::new();
         open_options
             .write(true)
             .create(true)
             .truncate(false)
             .mode(0o555);
-        drop(open_options.open("test_assets/unwritable")?);
-        test_err!("OPEN_W_FAILED", "test_assets/unwritable.bf");
-        remove_file("test_assets/unwritable.bf")?;
-        remove_file("test_assets/unwritable")?;
+        drop(open_options.open(&unwritable_dest)?);
+        test_err!("OPEN_W_FAILED", unwritable_src.as_os_str().to_str().unwrap());
         Ok(())
     }
 }
