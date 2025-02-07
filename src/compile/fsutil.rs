fd02faba1cb09d8f6d4144700adf1919ce2e7585
Eli Array Minkoff <eli@planetminkoff.com>
2025-02-07 17:38:04 -0500

    Add (untested) non-unix rm_ext implementation

--- src/compile/fsutil.rs
+++ src/compile/fsutil.rs
@@ -4,11 +4,13 @@
 
 use crate::err::{BFCompileError, BFErrorID};
 use std::ffi::OsStr;
+#[cfg(unix)]
 use std::os::unix::ffi::OsStrExt;
 
 /// if `filename` ends with `extension`, return `Ok(f)`, where `f` is `filename` without
 /// `extension` at the end.
 /// otherwise, returns `Err(filename)`
+#[cfg(unix)]
 pub(super) fn rm_ext<'a>(
     filename: &'a OsStr,
     extension: &OsStr,
@@ -28,6 +30,33 @@ pub(super) fn rm_ext<'a>(
             ),
         ))
     }
+}
+#[cfg(not(unix))]
+pub(super) fn rm_ext<'a>(
+    filename: &'a OsStr,
+    extension: &OsStr,
+) -> Result<&'a OsStr, BFCompileError> {
+    const SUPPORT_MSG: &str = " - non-Unicode filenames are only supported on Unix targets";
+    let filename = filename.to_str().ok_or(BFCompileError::basic(
+        BFErrorID::NonUTF8,
+        format!(
+            "filename {} is not valid Unicode{SUPPORT_MSG}",
+            filename.to_string_lossy()
+        ),
+    ))?;
+    let extension = extension.to_str().ok_or(BFCompileError::basic(
+        BFErrorID::NonUTF8,
+        format!(
+            "extension {} is not valid Unicode{SUPPORT_MSG}",
+            extension.to_string_lossy()
+        ),
+    ))?;
+    Ok(OsStr::new(filename
+        .strip_suffix(extension)
+        .ok_or(BFCompileError::basic(
+            BFErrorID::BadExtension,
+            format!("{filename} does not end with expected extension"),
+        ))?))
 }
 
 #[cfg(test)]
