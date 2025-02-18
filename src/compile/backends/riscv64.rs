5643e167de3260427a167f896f0a3563c90f82b4
Eli Array Minkoff <eli@planetminkoff.com>
2025-02-17 23:14:25 -0500

    Finish riscv implementation
    * Remove RiscVRegister::Zero, as it's not usable for most instrucitons,
      and is only ever used in set_zero
    * Use fits_within_bits or check if in range instead of using broken
      `min_bits`
    * Replace `T0` with `T2`, as the former is not usable in the `c.add`
      instruction, and would result in a special case
    * add a sign_extend utility function
    * adapt LLVM's code for handling the `li` pseudo-instruction

--- src/compile/backends/riscv64.rs
+++ src/compile/backends/riscv64.rs
@@ -1,60 +1,30 @@
 // SPDX-FileCopyrightText: 2025 Eli Array Minkoff
 //
-// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-License-Identifier: GPL-3.0-only AND Apache-2.0 WITH LLVM-exception
 
 use super::arch_inter::{ArchInter, FailableInstrEncoding, Registers, SyscallNums};
 use super::elf_tools::{ByteOrdering, ElfArch};
 use super::MinimumBits;
 use crate::err::{BFCompileError, BFErrorID};
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 #[repr(u8)]
 pub(in super::super) enum RiscVRegister {
     // read-only zero register
-    Zero = 0, // always-zero read-only register
-    T0 = 5,   // X5, scratch register
-    T1 = 6,   // X6, scratch register
-    S0 = 8,   // X8, bf pointer register
-    A0 = 10,  // X10, arg1 register
-    A1 = 11,  // X11, arg2 register
-    A2 = 12,  // X12, arg3 register
-    A7 = 17,  // X17, syscall register
+    T1 = 6,  // X6, scratch register
+    T2 = 7,  // X7, scratch register
+    S0 = 8,  // X8, bf pointer register
+    A0 = 10, // X10, arg1 register
+    A1 = 11, // X11, arg2 register
+    A2 = 12, // X12, arg3 register
+    A7 = 17, // X17, syscall register
 }
 
-// #[derive(Debug, Clone, Copy, PartialEq)]
-// #[repr(u8)]
-// /// Some RISC-V "C" extension compressed instructions use 3-bit rather than the normal 5-bit
-// /// encodings for the register operands, to save space, with the trade-off of not being able to
-// /// refer to all registers.
-// enum RiscVCompressedRegister {
-//     S0 = 0b000,
-//     A0 = 0b010,
-//     A1 = 0b011,
-//     A2 = 0b100,
-// }
-
-// impl TryFrom<RiscVRegister> for RiscVCompressedRegister {
-//     type Error = ();
-//     fn try_from(value: RiscVRegister) -> Result<Self, Self::Error> {
-//         value.compressed_form().ok_or(())
-//     }
-// }
-
 impl RiscVRegister {
-    // const fn compressed_form(self) -> Option<RiscVCompressedRegister> {
-    //     match self {
-    //         Self::S0 => Some(RiscVCompressedRegister::S0),
-    //         Self::A0 => Some(RiscVCompressedRegister::A0),
-    //         Self::A1 => Some(RiscVCompressedRegister::A1),
-    //         Self::A2 => Some(RiscVCompressedRegister::A2),
-    //         _ => None,
-    //     }
-    // }
     fn aux(self) -> Self {
-        if self == RiscVRegister::T0 {
+        if self == RiscVRegister::T2 {
             RiscVRegister::T1
         } else {
-            RiscVRegister::T0
+            RiscVRegister::T2
         }
     }
 }
@@ -90,7 +60,7 @@ macro_rules! encode_instr {
         validate_size!("opcode", 7, $op);
         validate_size!("funct3", 3, $funct3);
         assert!(
-            $imm.min_bits() <= 12,
+            $imm.fits_within_bits(12),
             "I-type expressions take 12-bit immediates"
         );
         u32::to_le_bytes(
@@ -105,7 +75,7 @@ macro_rules! encode_instr {
         validate_size!("opcode", 7, $op);
         validate_size!("funct3", 3, $funct3);
         assert!(
-            $imm.min_bits() <= 12,
+            $imm.fits_within_bits(12),
             "S-type expressions take 12-bit immediates"
         );
         u32::to_le_bytes(
@@ -119,14 +89,128 @@ macro_rules! encode_instr {
     }};
     ([U] $op: literal, $rd: expr, $imm: expr) => {{
         validate_size!("opcode", 6, $op);
-        assert_eq!(
-            $imm & 0xfff,
-            0,
-            "U-type instructions must have lowest 12 immediate bits set to 0"
+        assert!(
+            $imm.fits_within_bits(20),
+            "U-type instructions take 20-bit immediates"
         );
-        u32::to_le_bytes(($imm as u32 & !0xfff) | (($rd as u32) << 7) | $op)
+        u32::to_le_bytes((($imm as u32) << 12) | (($rd as u32) << 7) | $op)
+    }};
+    ([CI] $op: literal, $rd_rs1: expr, $funct3: literal, $imm: expr) => {{
+        validate_size!("opcode", 2, $op);
+        validate_size!("funct3", 3, $funct3);
+        assert!(
+            $imm.fits_within_bits(6),
+            "CI-type expressions take 6-bit immediates"
+        );
+        let imm = $imm as u16;
+        u16::to_le_bytes(
+            $funct3 << 13
+                | (imm & (1 << 5)) << 7
+                | (($rd_rs1 as u16) << 7)
+                | ((imm & 0x1f) << 2)
+                | $op,
+        )
     }};
 }
+
+/// Truncate `val` to `amnt` bits and sign extend the result
+const fn sign_extend(val: i64, amnt: u32) -> i64 {
+    val << (i64::BITS - amnt) >> (i64::BITS - amnt)
+}
+
+// SPDX-SnippetCopyrightText: 2025 Eli Array Minkoff
+//
+// SPDX-License-Identifier: GPL-3.0-only AND Apache-2.0 WITH LLVM-exception
+//
+// SPDX-SnippetCopyrightText: 2021 Alexander Pivovarov
+// SPDX-SnippetCopyrightText: 2021 Ben Shi
+// SPDX-SnippetCopyrightText: 2021 Craig Topper
+// SPDX-SnippetCopyrightText: 2021 Jim Lin
+// SPDX-SnippetCopyrightText: 2020 Simon Pilgrim
+// SPDX-SnippetCopyrightText: 2018 - 2019 Alex Bradbury
+// SPDX-SnippetCopyrightText: 2019 Chandler Carruth
+// SPDX-SnippetCopyrightText: 2019 Sam Elliott
+//
+// modification copyright 2025 Eli Array Minkoff
+//
+// And yes, I did have to go through the git commit history of the original file to find everyone
+// who had contributed as of 2022, because I didn't want to just go
+// "Copyright 2018-2022 LLVM Contributors"
+// and call it a day - I believe firmly in giving credit where credit is do.
+
+/// A modified port of LLVM's logic for resolving the `li` (load immediate) pseudo-instruction,
+/// as it existed in 2022.
+fn encode_li(code_buf: &mut Vec<u8>, reg: RiscVRegister, val: i64) {
+    let lo12 = sign_extend(val, 12);
+    if val.fits_within_bits(32) {
+        let hi20 = ((val as u64).wrapping_add(0x800) >> 12) as i64 & 0xfff;
+        if hi20 != 0 {
+            if hi20.fits_within_bits(6) {
+                // C.LUI
+                code_buf.extend(encode_instr!([CI] 0b01, reg, 0b011, hi20 as u16));
+            } else {
+                // LUI
+                code_buf.extend(encode_instr!([U] 0b011_0111, reg, hi20));
+            }
+        }
+        match (lo12, hi20) {
+            (n, 0) if n.fits_within_bits(6) => {
+                // C.LI
+                code_buf.extend(encode_instr!([CI] 0b01, reg, 0b010, lo12));
+            }
+            (0, _) => (),
+            (n, _) if n.fits_within_bits(6) => {
+                // C.ADDIW
+                code_buf.extend(encode_instr!([CI] 0b01, reg, 0b001, lo12));
+            }
+            (_, 0) => {
+                // ADDI reg, zero, lo12
+                code_buf.extend(encode_instr!([I] 0b001_0011, reg, 0, 0, lo12));
+            }
+            _ => {
+                // ADDIW reg, reg, lo12
+                code_buf.extend(encode_instr!([I] 0b001_1011, reg, reg, 0, lo12));
+            }
+        }
+        return;
+    }
+    // Below this point, the code is more-or-less a straight translation of the original LLVM code,
+    // comments and all
+
+    // In the following, constants are processed from LSB to MSB but instruction emission is
+    // performed from MSB to LSB by recursively calling encode_li. In each recursion, first the
+    // lowest 12 bits are removed from the constant and the optimal shift amount, which can be
+    // greater than 12 bits if the constant is sparse, is determined. Then, the shifted remaining
+    // constant is processed recursively and gets emitted as soon as it fits into 32 bits. The
+    // emission of the shifts and additions is subsequently performed when the recursion returns.
+    let mut hi52 = ((val as u64 + 0x800) >> 12) as i64;
+    let mut shift_amount = hi52.trailing_zeros() + 12;
+    hi52 = sign_extend(hi52 >> (shift_amount - 12), 64 - shift_amount);
+    // If the remaining bits don't fit in 12 bits, we might be able to reduce the shift amount in
+    // order to use LUI which will zero the lower 12 bits.
+    if shift_amount > 12 && !hi52.fits_within_bits(12) && (hi52 << 12).fits_within_bits(32) {
+        // Reduce the shift amount and add zeros to the LSBs so it will match LUI.
+        shift_amount -= 12;
+        hi52 = ((hi52 as u64) << 12) as i64;
+    }
+    // Recursive call
+    encode_li(code_buf, reg, hi52);
+    // Generation of the instruction
+    if shift_amount.fits_within_bits(6) && shift_amount != 0 {
+        // C.SLLI
+        code_buf.extend(encode_instr!([CI] 0b10, reg, 0, shift_amount));
+    } else {
+        // SLLI
+        code_buf.extend(encode_instr!([I] 0b001_0011, reg, reg, 0b111, shift_amount));
+    }
+    if lo12 != 0 && lo12.fits_within_bits(6) {
+        code_buf.extend(c_addi(reg, lo12 as i8));
+    } else if lo12 != 0 {
+        code_buf.extend(addi(reg, lo12 as i16));
+    }
+}
+
+// SPDX-SnippetEnd
 
 pub(crate) struct RiscV64Inter;
 
@@ -147,10 +231,10 @@ fn cond_jump(
 ) -> Result<[u8; 8], BFCompileError> {
     assert!(
         distance & 1 == 0,
-        "<...>::riscv64::cond_jump distance offset must be even"
+        "<…>::riscv64::cond_jump distance offset must be even"
     );
     distance >>= 1;
-    if distance.min_bits() > 12 {
+    if !distance.fits_within_bits(12) {
         return Err(BFCompileError::basic(
             BFErrorID::JumpTooLong,
             "Jump too long for riscv64 backend",
@@ -166,7 +250,7 @@ fn cond_jump(
     // make sure its in range, so it needs to take constant values
     macro_rules! branch {
         ($cond_code: literal) => {{
-            encode_instr!([S] 0b110_0011, aux, RiscVRegister::Zero, $cond_code, dist)
+            encode_instr!([S] 0b110_0011, aux, 0, $cond_code, dist)
         }}
     }
     let mut code = [0; 8];
@@ -182,25 +266,22 @@ fn cond_jump(
 
 fn c_addi(reg: RiscVRegister, i: i8) -> [u8; 2] {
     assert!(
-        i.min_bits() <= 6,
+        i.fits_within_bits(6),
         "c_addi must only be called with 6-bit signed immediates"
     );
     // C.ADDI (expands to `addi reg, reg, imm`, and reg and imm must not be zero
-    assert!(
-        i != 0 && reg != RiscVRegister::Zero,
-        "C.ADDI requires nonzero arguments"
-    );
-    let mut instr = [(reg as u8 & 1) << 7 | 0b0101, (reg as u8) >> 1];
-    instr[1] |= ((i as u8) & 0b100_000) >> 1;
-    instr[0] |= ((i as u8) & 0b11_111) << 2;
-    instr
+    assert!(i != 0, "C.ADDI requires nonzero arguments");
+    let imm = i16::from(i) as u16;
+    u16::to_le_bytes(0x0001 | (imm & (1 << 5)) << 7 | ((reg as u16) << 7) | ((imm & 0x1f) << 2))
 }
 
 fn store_to_byte(addr: RiscVRegister, src: RiscVRegister) -> [u8; 4] {
+    // SB
     encode_instr!([S] 0b100_011, addr, src, 0, 0)
 }
 
 fn load_from_byte(addr: RiscVRegister, dst: RiscVRegister) -> [u8; 4] {
+    // LB
     encode_instr!([I] 0b000_011, dst, addr, 0, 0)
 }
 
@@ -224,41 +305,11 @@ impl ArchInter for RiscV64Inter {
     const EI_DATA: ByteOrdering = ByteOrdering::LittleEndian;
 
     fn set_reg(code_buf: &mut Vec<u8>, reg: Self::RegType, imm: i64) {
-        match imm.min_bits() {
-            1..=6 => {
-                // C.LI reg (expands to addi rd, x0, imm[5:0])
-                let high_bit = ((imm & (1 << 6)) << 6) as u16;
-                code_buf.extend(u16::to_le_bytes(
-                    0x4001 | high_bit | ((reg as u16) << 7) | (imm & 0x3f << 2) as u16,
-                ));
-            }
-            0 | 7..=12 => {
-                // the LI pseudoinstruction is implemented as ADDI rd, zero, imm
-                code_buf.extend(encode_instr!([I] 0b001_0011, reg, RiscVRegister::Zero, 0, imm));
-            }
-            13..=18 => {
-                let high_bit = ((imm & (1 << 17)) >> 5) as u16;
-                // C.LUI reg (expands to addi rd, x0, imm[5:0])
-                code_buf.extend(u16::to_le_bytes(
-                    0x6001 | high_bit | ((reg as u16) << 7) | (imm & 0xf0_00 >> 10) as u16,
-                ));
-                Self::add_reg(code_buf, reg, (imm & 0xfff) as u64);
-            }
-            19..=32 => {
-                // this sign-extends the lowest 12 bits
-                let lower_12 = ((imm as i32) & 0xfff) << 20 >> 20;
-                let upper_20 = ((imm as i32) - lower_12) >> 12 << 12;
-                // LUI reg, imm&!0xfff
-                code_buf.extend(encode_instr!([U] 0b011_0111, reg, upper_20));
-                if lower_12 != 0 {
-                    Self::add_reg(code_buf, reg, i64::from(lower_12) as u64);
-                }
-            }
-            _ => todo!(),
-        }
+        encode_li(code_buf, reg, imm);
     }
 
     fn reg_copy(code_buf: &mut Vec<u8>, dst: Self::RegType, src: Self::RegType) {
+        // C.MV src, dst
         code_buf.extend(u16::to_le_bytes(
             0x8002 | (dst as u16) << 7 | (src as u16) << 2,
         ));
@@ -297,7 +348,7 @@ impl ArchInter for RiscV64Inter {
     fn sub_byte(code_buf: &mut Vec<u8>, reg: Self::RegType, imm: u8) {
         let aux = reg.aux();
         code_buf.extend(load_from_byte(reg, aux));
-        if (imm as i8).min_bits() <= 6 {
+        if (imm as i8).fits_within_bits(6) {
             code_buf.extend(c_addi(reg, -(imm as i8)));
         } else {
             code_buf.extend(addi(reg, -i16::from(imm)));
@@ -312,7 +363,7 @@ impl ArchInter for RiscV64Inter {
     fn add_byte(code_buf: &mut Vec<u8>, reg: Self::RegType, imm: u8) {
         let aux = reg.aux();
         code_buf.extend(load_from_byte(reg, aux));
-        if (imm as i8).min_bits() <= 6 {
+        if (imm as i8).fits_within_bits(6) {
             code_buf.extend(c_addi(reg, imm as i8));
         } else {
             code_buf.extend(addi(reg, i16::from(imm)));
@@ -321,14 +372,17 @@ impl ArchInter for RiscV64Inter {
     }
 
     fn add_reg(code_buf: &mut Vec<u8>, reg: Self::RegType, imm: u64) {
-        match (imm as i64).min_bits() {
+        match imm as i64 {
             0 => (),
-            1..=6 => code_buf.extend(c_addi(reg, imm as i8)),
-            7..=12 => code_buf.extend(addi(reg, imm as i16)),
-            13.. => {
+            -32..0 | 1..32 => code_buf.extend(c_addi(reg, imm as i8)),
+            -2048..-32 | 32..2048 => code_buf.extend(addi(reg, imm as i16)),
+            _ => {
                 let aux = reg.aux();
                 Self::set_reg(code_buf, aux, imm as i64);
-                code_buf.extend(encode_instr!([R] 0b011_0011, reg, reg, aux, 0, 0));
+                // C.ADDI reg, aux
+                code_buf.extend(u16::to_le_bytes(
+                    0x9002 | (reg as u16) << 7 | (aux as u16) << 2,
+                ));
             }
         }
     }
@@ -358,7 +412,8 @@ impl ArchInter for RiscV64Inter {
     }
 
     fn zero_byte(code_buf: &mut Vec<u8>, reg: Self::RegType) {
-        code_buf.extend(store_to_byte(reg, RiscVRegister::Zero));
+        // SB reg, zero
+        code_buf.extend(encode_instr!([S] 0b100_011, reg, 0, 0, 0));
     }
 }
 
@@ -375,6 +430,31 @@ mod test {
     }
 
     #[disasm_test]
+    fn test_set_reg() {
+        let mut v = Vec::new();
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::A0, 0);
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::A1, 1);
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::A2, -2);
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::T1, 0x123);
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::T2, -0x123);
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::S0, 0x100_000);
+        RiscV64Inter::set_reg(&mut v, RiscVRegister::A7, 0x123_456);
+        assert_eq!(
+            disassembler().disassemble(v),
+            [
+                "li a0, 0x0",
+                "li a1, 0x1",
+                "li a2, -0x2",
+                "li t1, 0x123",
+                "li t2, -0x123",
+                "lui s0, 0x100",
+                "lui a7, 0x123",
+                "addiw a7, a7, 0x456",
+            ]
+        );
+    }
+
+    #[disasm_test]
     fn test_caddi() {
         let mut v = Vec::from(c_addi(RiscVRegister::A0, -1));
         v.extend(c_addi(RiscVRegister::A1, 0x1f));
@@ -387,23 +467,23 @@ mod test {
     #[disasm_test]
     fn test_addi() {
         assert_eq!(addi(RiscVRegister::A0, 1), [0x13, 0x05, 0x15, 0x00]);
-        let mut v = Vec::from(addi(RiscVRegister::T0, -0x789));
+        let mut v = Vec::from(addi(RiscVRegister::T2, -0x789));
         v.extend(addi(RiscVRegister::T1, 0x123));
         assert_eq!(
             disassembler().disassemble(v),
-            ["addi t0, t0, -0x789", "addi t1, t1, 0x123"]
+            ["addi t2, t2, -0x789", "addi t1, t1, 0x123"]
         );
     }
 
     #[test]
     #[should_panic = "c_addi must only be called with 6-bit signed immediates"]
     fn test_caddi_guard_positive() {
-        c_addi(RiscVRegister::T0, 0b0111_0000);
+        c_addi(RiscVRegister::T2, 0b0111_0000);
     }
     #[test]
     #[should_panic = "c_addi must only be called with 6-bit signed immediates"]
     fn test_caddi_guard_negative() {
-        c_addi(RiscVRegister::T0, -0b0111_0000);
+        c_addi(RiscVRegister::T2, -0b0111_0000);
     }
 
     #[disasm_test]
@@ -423,11 +503,11 @@ mod test {
 
     #[disasm_test]
     fn load_and_store() {
-        let mut v = Vec::from(load_from_byte(RiscVRegister::A0, RiscVRegister::T0));
-        v.extend(store_to_byte(RiscVRegister::A0, RiscVRegister::T0));
+        let mut v = Vec::from(load_from_byte(RiscVRegister::A0, RiscVRegister::T2));
+        v.extend(store_to_byte(RiscVRegister::A0, RiscVRegister::T2));
         assert_eq!(
             disassembler().disassemble(v),
-            ["lb t0, 0x0(a0)", "sb t0, 0x0(a0)"]
+            ["lb t2, 0x0(a0)", "sb t2, 0x0(a0)"]
         );
     }
 
