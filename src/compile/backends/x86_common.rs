67a882b1ba93fa4396a3f886a254ce5703f14739
Eli Array Minkoff <eli@planetminkoff.com>
2025-04-13 12:24:19 -0400

    Don't warn on unused member in i386-only builds

--- src/compile/backends/x86_common.rs
+++ src/compile/backends/x86_common.rs
@@ -21,6 +21,7 @@ pub(in super::super) enum X86Register {
     #[expect(dead_code, reason = "included for completeness's sake")]
     Ebp = 0b101,
     Esi = 0b110,
+    #[cfg_attr(not(feature = "x86_64"), expect(dead_code, reason = "only used in x86_64"))]
     Edi = 0b111,
 }
 
